811198a31f7cde194447493ec9e7cb8e3ed7b35e
Ibrahim Suhail Al-Yousefi <cluab11222@gmail.com>
2023-01-30 17:15:24 +0200

    add required changes

--- student.rb
+++ student.rb
@@ -1,7 +1,7 @@
 require './person'
 class Student < Person
   def initialize(age, classroom, name = 'Unknown', parent_permission: true)
-    super(age: age, name: name, parent_permission: parent_permission)
+    super(age, name, parent_permission: parent_permission)
     @classroom = classroom
   end
 
--- teacher.rb
+++ teacher.rb
@@ -2,7 +2,7 @@ require './person'
 
 class Teacher < Person
   def initialize(age, specialization, name = 'Unknown', parent_permission: true)
-    super(age: age, name: name, parent_permission: parent_permission)
+    super(age, name, parent_permission: parent_permission)
     @specialization = specialization
   end
 
