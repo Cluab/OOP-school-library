8f33e8554cf3765c1fe17cc974bf8becc4136452
Ibrahim Suhail Al-Yousefi <cluab11222@gmail.com>
2023-01-31 14:48:55 +0200

    update classes

--- person.rb
+++ person.rb
@@ -1,8 +1,15 @@
+class Nameable
+  def correct_name
+    raise NotImplementedError
+  end
+end
+
 class Person < Nameable
   attr_accessor :name, :age
   attr_reader :id
 
   def initialize(age, name = 'Unknown', parent_permission: true)
+    super()
     @id = Random.rand(1..1000)
     @name = name
     @age = age
@@ -24,14 +31,9 @@ class Person < Nameable
   end
 end
 
-class Nameable
-  def correct_name
-    raise NotImplementedError
-  end
-end
-
 class BaseDecorator < Nameable
   def initialize(nameable)
+    super()
     @nameable = nameable
   end
 
@@ -42,6 +44,13 @@ end
 
 class CapitalizeDecorator < BaseDecorator
   def correct_name
-    @nameable.upcase
+    @nameable.correct_name.upcase
+  end
+end
+
+class TrimmerDecorator < BaseDecorator
+  # trim so i has only 10 characters
+  def correct_name
+    @nameable.correct_name.slice(0, 10)
   end
 end
