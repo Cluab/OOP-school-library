0fc3b812be41dfa74b32a6cc94a5345658e475bc
Ibrahim Suhail Al-Yousefi <cluab11222@gmail.com>
2023-02-03 18:24:46 +0200

    added chmod 755 lib/main

--- lib/student.rb
+++ lib/student.rb
@@ -17,11 +17,3 @@ class Student < Person
     classroom.students.push(self) unless classroom.students.include?(self)
   end
 end
-
-# art = Classroom.new('Art')
-# alex = Student.new(19, art, 'Alex')
-# art.add_student(alex)
-# alex.classroom
-# alex.classroom.label
-# alex.classroom.students
-# alex.classroom.students[0].name
