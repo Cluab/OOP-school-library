d576cb99e8ca591d417737129ff338bcfe6d94e6
Ibrahim Suhail Al-Yousefi <cluab11222@gmail.com>
2023-02-03 17:44:11 +0200

    add create student and teacher methodes

--- lib/app.rb
+++ lib/app.rb
@@ -18,6 +18,10 @@ class App
   end
 
   def list_people
+    if @people.empty?
+      puts 'Always remember that you can add yourself by typing "3" at the main menu.'
+      return
+    end
     @people.each { |person| puts "[#{person.class}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}" }
   end
 
@@ -33,22 +37,39 @@ class App
     Rental.new(date, book, person)
   end
 
+  def create_student(age, name)
+    print 'Has parent permission? [Y/N]: '
+    parent_permission = gets.chomp
+    case parent_permission
+    when 'y' || 'Y'
+      parent_permission = true
+    when 'n' || 'N'
+      parent_permission = false
+    else
+      puts 'Invalid input'
+      return
+    end
+    @people.push(Student.new(age, name, parent_permission: parent_permission))
+  end
+
+  def create_teacher(age, name)
+    print 'Specialization: '
+    specialization = gets.chomp
+    @people.push(Teacher.new(age, specialization, name))
+  end
+
   def create_person
     print 'Do you want to create a student (1) or a teacher (2)? [Input the number]: '
-    person_type = gets.chomp.to_i
+    person_type = gets.chomp
     print 'Age: '
-    age = gets.chomp.to_i
+    age = gets.chomp
     print 'Name: '
-    name = gets.chomp.to_s
+    name = gets.chomp
     case person_type
     when '1'
-      print 'Has parent permission? [Y/N]: '
-      parent_permission = gets.chomp.downcase == 'y'
-      student(age, name, parent_permission: parent_permission)
+      create_student(age, name)
     when '2'
-      print 'Specialization: '
-      specialization = gets.chomp.to_s
-      teacher(age, name, specialization, parent_permission: parent_permission)
+      create_teacher(age, name)
     else
       puts 'Invalid option'
       nil
