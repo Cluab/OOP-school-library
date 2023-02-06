cc02f468f1573813b0483828f659f5e73088058e
Ibrahim Suhail Al-Yousefi <cluab11222@gmail.com>
2023-02-06 15:06:30 +0200

    refactored main file so it follows the s.o.l.i.d Principle

--- main.rb
+++ main.rb
@@ -1,74 +1,75 @@
 #!/usr/bin/env ruby
-# rubocop:disable
+
 require_relative './app'
-def introduction
-  puts ''
-  puts ''
-  puts 'Welcome to School Library App!'
-  puts ''
-  puts 'Please choose an option by entering a number:'
-  puts '1 - List all books'
-  puts '2 - List all people'
-  puts '3 - Create a person'
-  puts '4 - Create a book'
-  puts '5 - Create a rental'
-  puts '6 - List all rentals for a given person id'
-  puts '7 - Exit'
-end
 
-def opener(app)
-  introduction
-  option = gets.chomp.to_i
-  checker(option, app)
-end
-
-def options_one(option, app)
-  case option
-  when 1
-    app.list_books
-  when 2
-    app.list_people
-  when 3
-    app.create_person
+class SchoolLibrary
+  def initialize(app)
+    @app = app
   end
-  opener(app)
-end
 
-def options_two(option, app)
-  case option
-
-  when 4
-    app.create_book
-  when 5
-    app.create_rental
-  when 6
-    app.list_rentals
-  when 7
-    puts 'Thank you for using this app!'
+  def run
+    introduction
+    option = gets.chomp.to_i
+    handle_option(option)
   end
-  return unless option != 7
 
-  opener(app)
-end
+  private
 
-def checker(option, app)
-  if option >= 1 && option <= 3
-    options_one(option, app)
-    return
+  def introduction
+    puts ''
+    puts ''
+    puts 'Welcome to School Library App!'
+    puts ''
+    puts 'Please choose an option by entering a number:'
+    puts '1 - List all books'
+    puts '2 - List all people'
+    puts '3 - Create a person'
+    puts '4 - Create a book'
+    puts '5 - Create a rental'
+    puts '6 - List all rentals for a given person id'
+    puts '7 - Exit'
   end
-  if option >= 4 && option <= 7
-    options_two(option, app)
-    return
-  end
-  return unless option < 1 || option > 7
 
-  puts 'Invalid input'
-  opener(app)
+  def handle_option(option)
+    case option
+    when 1..3
+      handle_options_one(option)
+    when 4..7
+      handle_options_two(option)
+    else
+      puts 'Invalid input'
+    end
+    run unless option == 7
+  end
+
+  def handle_options_one(option)
+    case option
+    when 1
+      @app.list_books
+    when 2
+      @app.list_people
+    when 3
+      @app.create_person
+    end
+  end
+
+  def handle_options_two(option)
+    case option
+    when 4
+      @app.create_book
+    when 5
+      @app.create_rental
+    when 6
+      @app.list_rentals
+    when 7
+      puts 'Thank you for using this app!'
+    end
+  end
 end
 
 def main
   app = App.new
-  opener(app)
+  SchoolLibrary.new(app).run
 end
 
 main
